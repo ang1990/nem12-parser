22291368f3b7c8e09aa3064b81c75196261034e5
ang1990 <ang1990@hotmail.com>
2026-02-04 17:27:56 +0800

    feat: Combined full and low-mem mode parsing. Added answers to readme.md

--- README.md
+++ README.md
@@ -3,15 +3,14 @@ A take-home assignment to read NEM12-formatted files, and generate SQL statement
 
 # Installation
 
-Make sure you have the following installed first:
-- Python 3.12 or later
+Install Python 3.12 or later: `brew install python`
 
-Run `pip install -r requirements.txt` 
+Install the required Python packages: `pip install -r requirements.txt` 
 
 # Operation
 
 The parser receives CSV files containing NEM12-compliant lines,
-and outputs INSERT statements according to the meter_readings table schema.
+and outputs INSERT statements according to the meter_readings table schema to STDOUT.
 
 To run the parser on a file:
 
@@ -25,18 +24,48 @@ python main_parser.py nem_12_file.csv
 
 I chose the following technologies:
 1. Python 3.12 - Python is easy to develop in and offers extensive library support
-which trivialises many parts of the development process (CSV reading, SQL generation).
+which trivialises several well-established parts of the development process (CSV reading, SQL generation).
 
     I use the `csv` and `sqlalchemy` packages for CSV reading and SQL generation respectively.
 
 
 ### Q2. What would you have done differently if you had more time?
 
-1. Study the impact of the INSERT statements on the database.
+1. Better understand how NEM12 records are generated and used. Leading questions would be:
+   >- How do you deal with meter readings that are estimated and then corrected?
+   >- Can we assume weak ordering of the NEM12 records by NMI?
+2. Implement switches to handle deconflicting of the insert statements.
+   > The engineering issue arises from how to deal with the uniqueness constraint whilst keeping memory usage down.
+   >
+   > This solution opted for a compromise between the two. For big files it runs the chance of generating conflicting inserts.
+   > 
+   > As it is unclear to me how conflicting inserts should be handled outside of this script,
+   > I opted to introduce the ON DUPLICATE KEY UPDATE handling to the insert statement. This limited me to the MySQL dialect in my implementation.
+   > 
+   > If I had more time I could implement switches to remove this handling, or introduce handling to generate the same deconfliction using different SQL dialects (PostgreSQL, MariaDB)
+   > 
+   > An alternative considered was to implement a get_and_update behavior, but ultimately left out because the parser assumes no access to the DB. Additionally, this solution could run into data loss issues in high-concurrency environments.
+   
 
 ### Q3. What is the rationale for the design choices that you have made?
 
 The assessment specified that the files could be very large. In light of this
 I made some design decisions:
 
-- Files larger than 2GB are 
+> The CSV reader chosen iterates through each line without storing the information from the previous line.
+This lets us read the file without loading the entire file into memory.
+>
+> Meter readings with zero value are ignored to cut down on inserts. The assumption is that downstream can assume a value to be zero if no record is found. For billing purposes this should be sufficient.
+
+The table schema also indicates a uniqueness constraint of NMI + timestamp. This made for a couple of design considerations:
+
+> The parser maintains a mapping linking an NMI + timestamp to the respective consumption. 
+The parser populates this mapping as it parses the document.
+> 
+>   The idea is to deduplicate for statements that have the same NMI and timestamp by summing up the values.
+
+However, this runs the risk of this mapping becoming very large and overrunning local memory.
+
+> The parser thus tracks the size of this mapping. On reaching this limit, the parser immediately outputs the insert statements and clears the mapping.
+> 
+> To remove issues around possible conflicting inserts from within the file, the inserts are provided an ON DUPLICATE KEY UPDATE deconfliction behavior to sum all consumption values with the unique key.
--- main_parser.py
+++ main_parser.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 from itertools import batched
 from zoneinfo import ZoneInfo
 from decimal import Decimal
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, func
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.types import DateTime, Uuid, Numeric
 from sqlalchemy.dialects.mysql import insert
@@ -26,13 +26,18 @@ constraint meter_readings_unique_consumption unique ("nmi", "timestamp")
 """
 
 
-GB = 10 ** 9
 MINS_PER_DAY = 24 * 60
 AUS_TZ = ZoneInfo('Australia/Brisbane')  # seems this tz is recommended for AEST
 
 # change this to your DB URL, or point to the envvar for it
 DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///:memory:')
 INSERT_BATCH_SIZE = 100
+CONSUMPTION_MAPPING_SIZE_LIMIT = 10 ** 5
+
+
+def _is_decimal_value(s: str) -> bool:
+    parts = s.split('.')
+    return all(not part or part.isnumeric() for part in parts)
 
 
 class Base(DeclarativeBase):
@@ -48,11 +53,6 @@ class MeterReading(Base):
     consumption = Column(Numeric(precision=6, scale=3), nullable=False)
 
 
-def get_engine():
-    from sqlalchemy import create_engine
-    return create_engine(DATABASE_URL)
-
-
 def parse(filepath: str):
     """
         fp: File Pointer
@@ -63,38 +63,36 @@ def parse(filepath: str):
             - Aggregate the consumption values per NMI
         3. Generate SQL from the NMIs and consumptions
     """
-    file_size = os.path.getsize(filepath)
     with open(filepath, 'r') as file:
         reader = csv.reader(file, delimiter=',')
         header_row = next(reader)
         assert header_row[0] == '100', 'Header malformed or missing'
         assert header_row[1] == 'NEM12', 'Only NEM12 format is supported'
 
-        for meter_reading_batch in batched(get_meter_readings(reader, file_size), n=INSERT_BATCH_SIZE):
+        for meter_reading_batch in batched(get_meter_readings(reader), n=INSERT_BATCH_SIZE):
             insert_stmt = insert(MeterReading).values([{
                 'nmi': mr.nmi,
                 'timestamp': mr.timestamp,
                 'consumption': mr.consumption
             } for mr in meter_reading_batch])
-            compiled_statement = insert_stmt.compile(compile_kwargs={"literal_binds": True})
-            yield compiled_statement
+
+            on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
+                consumption=(MeterReading.consumption + insert_stmt.inserted.consumption)
+            )
+            compiled_statement = on_duplicate_key_stmt.compile(compile_kwargs={"literal_binds": True})
+            yield str(compiled_statement)
 
 
-def get_meter_readings(file_reader, file_size: int) -> Iterable[MeterReading]:
-    if file_size < 2 * GB:  # 2GB to keep memory usage down
-        consumption_records_agg = parse_records_with_aggregation(file_reader)
-        for nmi, timestamped_records in consumption_records_agg.items():
-            for timestamp, consumption in timestamped_records.items():
-                yield MeterReading(nmi=nmi, timestamp=timestamp, consumption=consumption)
-    else:
-        yield from parse_records_low_memory(file_reader)
+def get_meter_readings(file_reader) -> Iterable[MeterReading]:
+    yield from parse_records(file_reader)
 
 
-def parse_records_low_memory(file_reader):
-    curr_nmi: str = ''
-    time_interval: int = 0
+def parse_records(file_reader) -> Iterable[MeterReading]:
     consumption_records_agg = defaultdict(lambda: defaultdict(Decimal))
+    curr_nmi = ''
+    time_interval: int = 0
     records_count = 0
+
     for row in file_reader:
         match row[0]:
             case '200':
@@ -103,20 +101,18 @@ def parse_records_low_memory(file_reader):
                 time_interval = int(row[-2])
             case '300':
                 assert curr_nmi, '300 record encountered before 200 record'
-                # stick to this timezone so we don't have DST problems
                 interval_date = datetime.strptime(row[1], '%Y%m%d').astimezone(AUS_TZ)
-                interval_count = row.index(next(elem for elem in row[2:] if not elem.isnumeric())) - 2
+                interval_count = row.index(next(elem for elem in row[2:] if not _is_decimal_value(elem))) - 2
                 for idx in range(interval_count):
                     curr_timestamp = interval_date + timedelta(minutes=time_interval * idx)
-                    if reading := row[idx+2]:
-                        consumption = Decimal(reading)
+                    if reading := Decimal(row[idx+2]):
                         if consumption_records_agg[curr_nmi][curr_timestamp] > 0:
-                            consumption_records_agg[curr_nmi][curr_timestamp] += consumption
+                            consumption_records_agg[curr_nmi][curr_timestamp] += Decimal(reading)
                         else:
-                            consumption_records_agg[curr_nmi][curr_timestamp] = consumption
                             records_count += 1
+                            consumption_records_agg[curr_nmi][curr_timestamp] = Decimal(reading)
 
-                        if records_count >= INSERT_BATCH_SIZE:
+                        if records_count >= CONSUMPTION_MAPPING_SIZE_LIMIT:
                             for nmi, timestamped_records in consumption_records_agg.items():
                                 for timestamp, consumption in timestamped_records.items():
                                     yield MeterReading(nmi=nmi, timestamp=timestamp, consumption=consumption)
@@ -132,47 +128,25 @@ def parse_records_low_memory(file_reader):
             case '900':
                 break
 
-    # final batch to clear
+    # clear the remaining for low mem mode (or the whole thing for the agg mode)
     for nmi, timestamped_records in consumption_records_agg.items():
         for timestamp, consumption in timestamped_records.items():
             yield MeterReading(nmi=nmi, timestamp=timestamp, consumption=consumption)
-
-def parse_records_with_aggregation(file_reader) -> defaultdict:
-    consumption_records_agg = defaultdict(lambda: defaultdict(Decimal))
-    curr_nmi = ''
-    time_interval: int = 0
-
-    for row in file_reader:
-        match row[0]:
-            case '200':
-                # 200 statement, we switch NMI context
-                curr_nmi = row[1]
-                time_interval = int(row[-2])
-            case '300':
-                assert curr_nmi, '300 record encountered before 200 record'
-                interval_date = datetime.strptime(row[1], '%Y%m%d').astimezone(AUS_TZ)
-                interval_count = row.index(next(elem for elem in row[2:] if not elem.isnumeric())) - 2
-                for idx in range(interval_count):
-                    curr_timestamp = interval_date + timedelta(minutes=time_interval * idx)
-                    if reading := row[idx+2]:
-                        consumption_records_agg[curr_nmi][curr_timestamp] += Decimal(reading)
-            case '400':
-                # we don't need 400s yet
-                pass
-            case '500':
-                curr_nmi = ''
-                time_interval = 0
-                pass
-            case '900':
-                break
-    return consumption_records_agg
 
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath", help="The name of the file to parse.")
+    parser.add_argument('filepath', help='The name of the file to parse.')
+    parser.add_argument('--output_file', nargs='?', type=str, default='',
+                        help='Name of the file to write the insert statements to. '
+                             'Ignore if you do not want the parser to write to a file.'
+                             'Alternative you could pipe STDOUT to a file instead.')
     args = parser.parse_args()
-
-    for r in parse(args.filepath):
-        print(r)
+    if args.output_file:
+        with open(args.output_file, 'w') as output_file:
+            for insert_stmt in parse(args.filepath):
+                output_file.write(insert_stmt)
+    else:
+        for r in parse(args.filepath):
+            print(r)
