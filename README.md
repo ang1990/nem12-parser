a48cfb4183a8f73ae04de0e4411f29a81dac4940
ang1990 <ang1990@hotmail.com>
2026-02-04 14:31:24 +0800

    fix: Fixed CSV handling (they're all strings), include params in insert query

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # nem12-parser
 A take-home assignment to read NEM12-formatted files, and generate SQL statements representing the corresponding energy consumption records.
+
+# Installation
+
+Make sure you have the following installed first:
+- Python 3.12 or later
+
+Run `pip install -r requirements.txt` 
+
+# Operation
+
+The parser receives CSV files containing NEM12-compliant lines,
+and outputs INSERT statements according to the meter_readings table schema.
+
+To run the parser on a file:
+
+```bash
+python main_parser.py nem_12_file.csv
+```
+
+# Technical Assessment Answers
+
+### Q1. What is the rationale for the technologies you have decided to use?
+
+I chose the following technologies:
+1. Python 3.12 - Python is easy to develop in and offers extensive library support
+which trivialises many parts of the development process (CSV reading, SQL generation).
+
+    I use the `csv` and `sqlalchemy` packages for CSV reading and SQL generation respectively.
+
+
+### Q2. What would you have done differently if you had more time?
+
+1. Study the impact of the INSERT statements on the database.
+
+### Q3. What is the rationale for the design choices that you have made?
+
+The assessment specified that the files could be very large. In light of this
+I made some design decisions:
+
+- Files larger than 2GB are 
--- main_parser.py
+++ main_parser.py
@@ -6,13 +6,14 @@ from datetime import datetime, timedelta
 from itertools import batched
 from zoneinfo import ZoneInfo
 from decimal import Decimal
-from sqlalchemy import Column, String, func
-from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy import Column, String
+from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.types import DateTime, Uuid, Numeric
 from sqlalchemy.dialects.mysql import insert
 from typing import Iterable
 
 
+# meter_readings table schema for reference
 """
 create table meter_readings ( 
 id uuid default gen_random_uuid() not null, 
@@ -31,7 +32,7 @@ AUS_TZ = ZoneInfo('Australia/Brisbane')  # seems this tz is recommended for AEST
 
 # change this to your DB URL, or point to the envvar for it
 DATABASE_URL = os.environ.get('DATABASE_URL', 'sqlite:///:memory:')
-DB_BATCH_SIZE = 100
+INSERT_BATCH_SIZE = 100
 
 
 class Base(DeclarativeBase):
@@ -57,29 +58,25 @@ def parse(filepath: str):
         fp: File Pointer
         Algorithm:
         1. Read the header
-        2. Alternate between 200s and 300s
+        2. Read the 200s and 300s
             - Generate the NMIs and consumptions
             - Aggregate the consumption values per NMI
         3. Generate SQL from the NMIs and consumptions
     """
     file_size = os.path.getsize(filepath)
-    with open(filepath, "r") as file:
-        reader = csv.reader(file, delimiter=",")
+    with open(filepath, 'r') as file:
+        reader = csv.reader(file, delimiter=',')
         header_row = next(reader)
-        assert header_row[0] == 100, 'Header malformed or missing'
+        assert header_row[0] == '100', 'Header malformed or missing'
         assert header_row[1] == 'NEM12', 'Only NEM12 format is supported'
 
-        for meter_reading_batch in batched(get_meter_readings(reader, file_size), n=DB_BATCH_SIZE):
+        for meter_reading_batch in batched(get_meter_readings(reader, file_size), n=INSERT_BATCH_SIZE):
             insert_stmt = insert(MeterReading).values([{
                 'nmi': mr.nmi,
                 'timestamp': mr.timestamp,
                 'consumption': mr.consumption
             } for mr in meter_reading_batch])
-            on_duplicate_key_stmt = insert_stmt.on_duplicate_key_update(
-                consumption=func.sum(func.distinct(insert_stmt.inserted.consumption))
-            )
-            compiled_statement = on_duplicate_key_stmt.compile()
-            print(compiled_statement)
+            compiled_statement = insert_stmt.compile(compile_kwargs={"literal_binds": True})
             yield compiled_statement
 
 
@@ -100,15 +97,15 @@ def parse_records_low_memory(file_reader):
     records_count = 0
     for row in file_reader:
         match row[0]:
-            case 200:
+            case '200':
                 # 200 statement, we switch NMI context
                 curr_nmi = row[1]
-                time_interval = row[-2]
-            case 300:
+                time_interval = int(row[-2])
+            case '300':
                 assert curr_nmi, '300 record encountered before 200 record'
                 # stick to this timezone so we don't have DST problems
                 interval_date = datetime.strptime(row[1], '%Y%m%d').astimezone(AUS_TZ)
-                interval_count = row.index(next(elem for elem in row if elem.isalpha())) - 2
+                interval_count = row.index(next(elem for elem in row[2:] if not elem.isnumeric())) - 2
                 for idx in range(interval_count):
                     curr_timestamp = interval_date + timedelta(minutes=time_interval * idx)
                     if reading := row[idx+2]:
@@ -119,22 +116,26 @@ def parse_records_low_memory(file_reader):
                             consumption_records_agg[curr_nmi][curr_timestamp] = consumption
                             records_count += 1
 
-                        if records_count >= DB_BATCH_SIZE:
+                        if records_count >= INSERT_BATCH_SIZE:
                             for nmi, timestamped_records in consumption_records_agg.items():
                                 for timestamp, consumption in timestamped_records.items():
                                     yield MeterReading(nmi=nmi, timestamp=timestamp, consumption=consumption)
                             consumption_records_agg.clear()
                             records_count = 0
-            case 400:
-                # we don't have a use case for 400 yet
+            case '400':
+                # we don't need 400s yet
                 pass
-            case 500:
+            case '500':
                 curr_nmi = ''
                 time_interval = 0
                 pass
-            case 900:
+            case '900':
                 break
 
+    # final batch to clear
+    for nmi, timestamped_records in consumption_records_agg.items():
+        for timestamp, consumption in timestamped_records.items():
+            yield MeterReading(nmi=nmi, timestamp=timestamp, consumption=consumption)
 
 def parse_records_with_aggregation(file_reader) -> defaultdict:
     consumption_records_agg = defaultdict(lambda: defaultdict(Decimal))
@@ -143,28 +144,27 @@ def parse_records_with_aggregation(file_reader) -> defaultdict:
 
     for row in file_reader:
         match row[0]:
-            case 200:
+            case '200':
                 # 200 statement, we switch NMI context
                 curr_nmi = row[1]
-                time_interval = row[-2]
-            case 300:
+                time_interval = int(row[-2])
+            case '300':
                 assert curr_nmi, '300 record encountered before 200 record'
                 interval_date = datetime.strptime(row[1], '%Y%m%d').astimezone(AUS_TZ)
-                interval_count = row.index(next(elem for elem in row if elem.isalpha())) - 2
+                interval_count = row.index(next(elem for elem in row[2:] if not elem.isnumeric())) - 2
                 for idx in range(interval_count):
                     curr_timestamp = interval_date + timedelta(minutes=time_interval * idx)
                     if reading := row[idx+2]:
                         consumption_records_agg[curr_nmi][curr_timestamp] += Decimal(reading)
-            case 400:
-                # we don't have a use case for 400 yet
+            case '400':
+                # we don't need 400s yet
                 pass
-            case 500:
+            case '500':
                 curr_nmi = ''
                 time_interval = 0
                 pass
-            case 900:
+            case '900':
                 break
-
     return consumption_records_agg
 
 
@@ -173,4 +173,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("filepath", help="The name of the file to parse.")
     args = parser.parse_args()
-    parse(filepath=args.filepath)
+
+    for r in parse(args.filepath):
+        print(r)
